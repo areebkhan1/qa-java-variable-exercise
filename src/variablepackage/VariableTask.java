2e9b09c27ca88382ccdf939c247bfda58b87745f
areebkhan1 <ak1657201@gmail.com>
2023-09-15 11:38:22 +0100

    variable exercise 15/07

---
+++ src/variablepackage/VariableTask.java
@@ -0,0 +1,43 @@
+package variablepackage;
+import java.util.Scanner;
+
+public class VariableTask {
+    public static void main(String[] args) {
+       /* String firstName;
+        String lastName;
+
+        firstName = "Areeb";
+        lastName = "Khan";
+
+        String name = firstName + lastName;
+
+        System.out.println(name);
+
+        */
+
+        /*int num1 = 3;
+        int num2 = 4;
+        System.out.println(num1+num2);
+
+        float result = (float) num1/num2;
+        System.out.println(result);
+
+        int num3 = 8;
+
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3); */
+
+Scanner testScanner = new Scanner(System.in);
+        System.out.println("Enter your first and surname");
+String nameSurname = testScanner.nextLine();
+System.out.println(nameSurname);
+
+        System.out.println("Enter a number");
+int num1 = testScanner.nextInt();
+        System.out.println("enter another number");
+int num2 = testScanner.nextInt();
+
+System.out.println(num1+num2);
+    }
+}
